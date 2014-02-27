3abebd3f40c6bca2b11b72de216ffbb18991d5d9
Luís Fonseca <miguelluisfonseca@gmail.com>
2014-02-27 15:23:41 +0000

    Added new task create to generate Android default files and a gitignore.

---
+++ src/main/scala/Create.scala
@@ -0,0 +1,93 @@
+import java.io.File
+
+object Create {
+
+  val androidManifestFile = new File("src\\main\\AndroidManifest.xml")
+  val valuesStringFile = new File("src\\main\\res\\values\\string.xml")
+  val layoutMainFile = new File("src\\main\\res\\layout\\main.xml")
+  val gitignoreFile = new File(".gitignore")
+
+  
+  def directoriesWith(packageName: String, minApiLevel: Int) = {
+    
+      val commonDirectories = Seq[String](
+        "src\\main\\res\\values",
+        "src\\main\\res\\layout",
+        "src\\main\\res\\menu",
+        "src\\test\\scala",
+        "src\\main\\scala\\" + packageName.replace('.', '\\')
+      )
+
+      val allDirectories =
+        if (minApiLevel < 14)
+          commonDirectories
+        else
+          commonDirectories ++ Seq[String](
+            "src\\main\\res\\drawable-hdpi",
+            "src\\main\\res\\drawable-ldpi",
+            "src\\main\\res\\drawable-mdpi",
+            "src\\main\\res\\drawable-xhdpi"
+          )
+
+      allDirectories map(new File(_))
+  }
+
+
+  def manifestXML(packageName: String, minApiLevel: Int) =
+    """<?xml version="1.0" encoding="utf-8"?>
+       |<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+       |      package="%s"
+       |      android:versionCode="1"
+       |      android:versionName="1.0">
+	   |    <uses-sdk android:minSdkVersion="%d" />
+       |    <application android:label="@string/app_name" android:icon="@drawable/ic_launcher">
+       |        <activity android:name="MainActivity"
+       |                  android:label="@string/app_name">
+       |            <intent-filter>
+       |                <action android:name="android.intent.action.MAIN" />
+       |                <category android:name="android.intent.category.LAUNCHER" />
+       |            </intent-filter>
+       |        </activity>
+       |    </application>
+       |</manifest>
+       |""".stripMargin.format(packageName, minApiLevel)
+
+  def valuesStringXML =
+    """<?xml version="1.0" encoding="utf-8"?>
+       |<resources>"
+       |    <string name="app_name">MainActivity</string>
+       |""".stripMargin
+
+
+  def layoutMainXML =
+    """<?xml version="1.0" encoding="utf-8"?>
+       |<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+       |    android:orientation="vertical"
+       |    android:layout_width="fill_parent"
+       |    android:layout_height="fill_parent"
+	   |    >
+       |<TextView
+       |    android:layout_width="fill_parent"
+       |    android:layout_height="wrap_content"
+       |    android:text="Hello World, from MainActivity :)"
+       |    />
+       |</LinearLayout>
+       |""".stripMargin
+
+
+  def defaultGitIgnore =
+    """|# built application files
+       |*.apk
+       |*.ap_
+       |*.dex
+	   |*.class
+       |*.o
+       |*.so
+       |
+       |# generated files
+       |target/
+       |
+	   |# Mac OS X clutter
+	   |*.DS_Store
+	   |""".stripMargin
+}
--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -13,6 +13,7 @@ object Plugin extends Plugin {
   object AgileAndroidKeys {
 	  val generate = inputKey[File]("Generates stuff.")
     
+    val create = inputKey[Unit]("Generates a new android project.")
 	
 	  def generateTask: Initialize[InputTask[File]] = Def.inputTask {
       val args: Seq[String] = spaceDelimited("  className <attributes>").parsed
@@ -27,9 +28,38 @@ object Plugin extends Plugin {
 	    file
 	  }
 
+    def createTask: Initialize[InputTask[Unit]] = Def.inputTask {
+      val args: Seq[String] = spaceDelimited("package minApiLevel").parsed
+      if (args.length < 2) {
+        sys.error("Incorrect parameters.")
+      }
+      val packageName = args(0)
+      val minApiLevel = args(1).toInt
+
+      val directories = Create.directoriesWith(packageName, minApiLevel)
+
+      //IO.write(buildProps, "sbt.version=%s\n" format sbtVersion.value)
+      //streams.value.log.info("Generated build properties")
+
+      IO.write(Create.androidManifestFile, Create.manifestXML(packageName, minApiLevel))
+      streams.value.log.info("Generated manifest file.")
+
+      IO.createDirectories(directories)
+      streams.value.log.info("Generated source directories.")
+
+      IO.write(Create.valuesStringFile, Create.valuesStringXML)
+      IO.write(Create.layoutMainFile, Create.layoutMainXML)
+      // Todo: class
+      streams.value.log.info("Generated source files.")
+
+      IO.write(Create.gitignoreFile, Create.defaultGitIgnore)
+      streams.value.log.info("Generated gitignore.")
+    }
+
     // a group of settings ready to be added to a project
     val defaultAgileAndroidSettings : Seq[sbt.Def.Setting[_]] = Seq(
-	    generate := generateTask.evaluated
+	    generate := generateTask.evaluated,
+      create := createTask.evaluated
     )
   }
 }
