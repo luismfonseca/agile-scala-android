b10e182ddd3fc30a394c5616891e88cc606d6f79
Luís Fonseca <miguelluisfonseca@gmail.com>
2014-03-03 18:19:44 +0000

    Added check for missing manifest file.

--- src/main/scala/Android.scala
+++ src/main/scala/Android.scala
@@ -5,7 +5,18 @@ import scala.xml._
 
 object Android
 {
-	def findManifestPath(sourceDirectory: File): File = new File((sourceDirectory ** "AndroidManifest.xml").getPaths(0))
+	def findManifestPath(sourceDirectory: File): File =
+	{
+	  try
+	  {
+	    new File((sourceDirectory ** "AndroidManifest.xml").getPaths(0))
+	  }
+	  catch
+	  {
+	    case _ : Throwable => throw new Exception("""Manifest file was not found!
+                                                    |Is this an Android project?""".stripMargin)
+	  }
+	}
 
 	def findPackageName(sourceDirectory: File) =
 	{
